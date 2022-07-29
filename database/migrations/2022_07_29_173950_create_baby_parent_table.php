f1ab7f3f6e8a10fbd937a193ad7cb822578df3e2
Eyad Hamza <EyadHamza0@outlook.com>
2022-07-29 17:40:46 +0200

    basic models structure

---
+++ database/migrations/2022_07_29_173950_create_baby_parent_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('baby_parent', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('baby_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('parent_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('baby_parent');
+    }
+};
